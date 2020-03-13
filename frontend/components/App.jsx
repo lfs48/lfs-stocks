d587e425b69f3fedef8f7a9c9a7d2e28fbadf4cb
lucas schraier <lucasfschraier@gmail.com>
2020-03-13 13:11:18 -0400

    Remove home route, add portfolio and transactions routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import Navbar from './navbar/navbar';
 import Splash from './splash/splash';
-import Home from './home/home';
 import { AuthRoute, ProtectedRoute } from './routes/routes';
+import Portfolio from './portfolio/portfolio';
+import Transactions from './transactions/transactions';
 
 const App = () => (
     <main id="app-container">
         <Navbar/>
-        <ProtectedRoute path="/" component={Home} />
+        <ProtectedRoute path="/transactions" component={Transactions} />
+        <ProtectedRoute path="/" component={Portfolio} />
         <AuthRoute path="/" component={Splash} />
     </main>
 );

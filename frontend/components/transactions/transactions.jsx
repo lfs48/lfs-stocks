1b370978b55632a172d534b5f0f56cdd22b7ba65
lucas schraier <lucasfschraier@gmail.com>
2020-03-13 19:04:26 -0400

    Render list of user's transactions

--- frontend/components/transactions/transactions.jsx
+++ frontend/components/transactions/transactions.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import { fetchUserTransactions } from '../../actions/entities/transaction_actions';
 
 const Transactions = () => {
-    return(<></>);
+
+    const dispatch = useDispatch();
+
+    const {transactions, currentUser} = useSelector(
+        state => ({
+            currentUser: state.entities.users[state.sessions.id],
+            transactions: state.entities.transactions
+        })
+    )
+
+    useEffect( () => {
+        dispatch(fetchUserTransactions(currentUser.id))
+    },
+    []);
+
+    const lis = Object.values(transactions).filter(transaction => transaction.purchaser_id === currentUser.id).map( (transaction) => {
+        return(
+            <li key={transaction.id}>
+                <span>BUY {transaction.ticker}</span>
+                <span>{transaction.shares} Shares @ {transaction.price}</span>
+            </li>
+        );
+    });
+
+
+    return(
+        <section id="transactions-container">
+
+            <ul>
+                {lis}
+            </ul>
+
+        </section>
+    );
 }
 
 export default Transactions;

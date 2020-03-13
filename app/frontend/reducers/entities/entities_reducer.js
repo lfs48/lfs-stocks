ad6ff48f7be0aa408fe7d315efb1d7c50ef23463
lucas schraier <lucasfschraier@gmail.com>
2020-03-13 15:31:32 -0400

    Delete duplicate files

--- app/frontend/reducers/entities/entities_reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import usersReducer from './users_reducer';
-
-// Combines all lower-level entities reducers, to be passed to the root reducer under the 'entities' key.
-const entitiesReducer = combineReducers({
-    users: usersReducer
-});
-
-export default entitiesReducer;

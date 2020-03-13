890d680cd68907435e2685d46c0ac355b88ba2b6
lucas schraier <lucasfschraier@gmail.com>
2020-03-13 15:32:55 -0400

    Add stocks routes nested under users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: "static_pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :create, :show]
+    resources :users, only: [:index, :create, :show] do
+      resources :stocks, only: [:index, :create, :update, :destroy]
+    end
     resource :session, only: [:create, :destroy]
   end
 

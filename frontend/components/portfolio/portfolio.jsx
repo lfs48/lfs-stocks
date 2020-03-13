e44636f8bc080fcec206bed8b319dd163e791d26
lucas schraier <lucasfschraier@gmail.com>
2020-03-13 13:17:44 -0400

    Render ticker and quantity state-controlled input fields

--- frontend/components/portfolio/portfolio.jsx
+++ frontend/components/portfolio/portfolio.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {merge} from 'lodash';
 
 const Portfolio = () => {
-    return(<></>);
+
+    const [state, setState] = useState({
+        ticker: "",
+        quantity: ""
+    });
+
+    const updateInput = (e, field) => {
+        e.preventDefault();
+        const newState = merge({}, state);
+        newState[field] = e.target.value;
+        setState(newState);
+    }
+
+    return(
+        <section id="portfolio-container">
+
+            <section id="purchase-container">
+            <input 
+                type="text" 
+                id="ticker-input" 
+                placeholder="Ticker"
+                value={state.ticker}
+                onChange={e => updateInput(e, "ticker")}
+            ></input>
+
+            <input 
+                type="number" 
+                id="qty-input" 
+                placeholder="Quantity"
+                value={state.quantity}
+                onChange={e => updateInput(e, "quantity")}
+            ></input>
+
+            </section>
+            
+
+        </section>
+    );
 }
 
 export default Portfolio;

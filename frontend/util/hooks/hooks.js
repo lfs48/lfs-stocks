9a74bf95178a7a75315ed57366c585003f13932c
lucas schraier <lucasfschraier@gmail.com>
2020-03-15 23:37:39 -0400

    Add useInterval hook for using intervals within functional components

---
+++ frontend/util/hooks/hooks.js
@@ -0,0 +1,23 @@
+import {useRef, useEffect} from 'react';
+
+// Taken from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+// React hook to allow use of setInterval
+export const useInterval = (callback, delay) => {
+    const savedCallback = useRef();
+  
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+  
+    // Set up the interval.
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+}
